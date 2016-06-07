f33fe54283e19248f2fbd42d095ca5372bedcc6b
lichongyang <lichongyang@zuoyebang.com>
2016-06-07 16:36:41 +0800

    update the file

--- Softs/diordna/diordna_migwin.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-path=`dirname $0`
-unzip $1 classes.dex -d ./diordna/
-$path/dex2jar/d2j-dex2jar.sh -f ./diordna/classes.dex -o ./diordna/classes_dex2jar.jar
-$path/jd-gui.exe ./diordna/classes_dex2jar.jar
